c83a8973413bdbf6b56390a10b5e679698293247
MucheruM <petermucheru420@gmail.com>
2023-12-18 12:05:47 -0800

    0x05-python-exceptions codes

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(0, list_length):
@@ -16,4 +15,4 @@ def list_division(my_list_1, my_list_2, list_length):
             div = 0
         finally:
             new_list.append(div)
-        return (new_list)
+    return (new_list)
