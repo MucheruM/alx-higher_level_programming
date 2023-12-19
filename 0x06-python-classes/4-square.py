d3bd1cd436f69ea7197d10745f772358d60ee105
MucheruM <petermucheru420@gmail.com>
2023-12-19 13:56:19 -0800

    0x06-python-classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" define a class named Square """
+
+
+class Square:
+    """ Represent a Square """
+
+    def __init__(self, size=0):
+        """
+        Initialize a new Square
+
+        Args:
+             size (int): The size of the new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstances(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Return the area of the square"""
+        return (self.__size * self.__size)
