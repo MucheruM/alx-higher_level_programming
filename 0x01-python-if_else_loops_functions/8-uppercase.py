1c0bda8a400675336d1507e72814f1462ed07d70
MucheruM <petermucheru420@gmail.com>
2024-01-02 23:54:09 -0800

    Convert str in lowercase into uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for i in str:
-        if ord("a") <= ord(i) <= ord("z"):
-            i = chr(ord(i) - 32)
-        print("{:s}".format(i), end="")
+"""
+   A function defination in python using def
+   We can also say
+   if ord(ch) <= 97 and ord(ch) <= 122
+   print(f"{ch}", end='')
+   We can also add a call function i.e.
+   uppercase(str)
+   str = input("Enter your string: ")
+"""
+
+def uppercase(str): #Convert str from uppercase to lowercase
+    for i in str: #Look at chr in str and modify them individually
+        if ord("a") <= ord(i) <= ord("z"): #We check for the ASCII values ord()
+            i = chr(ord(i) - 32) #Convert them into UPPERCASE
+        print("{:s}".format(i), end="") #Print the result
     print()
