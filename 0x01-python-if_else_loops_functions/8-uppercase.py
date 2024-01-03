8b2fcb21dafb8d0bc002b804125f78117a88ceaa
MucheruM <petermucheru420@gmail.com>
2024-01-03 00:18:01 -0800

    convert lowercase to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-"""
-   A function defination in python using def
-   We can also say
-   if ord(ch) <= 97 and ord(ch) <= 122
-   print(f"{ch}", end='')
-   We can also add a call function i.e.
-   uppercase(str)
-   str = input("Enter your string: ")
-"""
 
-def uppercase(str):
+def uppercase(str): """Function definition"""
     for i in str:
         if ord("a") <= ord(i) <= ord("z"):
-            i = chr(ord(i) - 32)
-        print("{:s}".format(i), end="")
+            i = chr(ord(i) - 32) """Converting @ char"""
+        print("{:s}".format(i), end="") """Printing the result"""
     print()
