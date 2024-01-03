5ca3e28143566d248e3f920fbefa7e0a1c344ac6
MucheruM <petermucheru420@gmail.com>
2024-01-03 01:03:13 -0800

    Convert lowercase into uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@
 def uppercase(str):
     for i in str:
         if ord("a") <= ord(i) <= ord("z"):
-            i = chr(ord(i) - 32) """Converting @ char"""
-        print("{:s}".format(i), end="") """Printing the result"""
+            i = chr(ord(i) - 32)
+        print("{:s}".format(i), end="")
     print()
