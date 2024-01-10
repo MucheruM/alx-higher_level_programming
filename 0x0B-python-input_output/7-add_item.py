89d0d058e55394311245c7df52219ec55a80b4c8
MucheruM <petermucheru420@gmail.com>
2024-01-09 18:17:27 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,7 @@ import sys
 
 
 if __name__ == "__main__":
+    """ Add arguments to a python list and save"""
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
