c0b072e7adb4edf52c87409f8266e2ced1a77407
MucheruM <petermucheru420@gmail.com>
2024-02-14 12:37:53 -0800

    Write a script that displays temperatures in top 3 cities ordered by month 7 and 8

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,13 @@
 -- Displays top 3 cities temp during July and August DESC
+--
 SELECT city, AVG(value) AS avg_temp
+-- Specifies the table from which to retrieve data
 FROM `temperatures`
+-- Filter the data for months 7(July) and 8(August)
 WHERE month IN (7, 8)
+-- Groups the result by city
 GROUP BY `city`
-GROUP BY `avg_temp`
-DESC
+-- Orders the result by avg temp in desc order
+GROUP BY avg_temp DESC
+-- Limits the output to the top 3 records
 LIMIT 3;
