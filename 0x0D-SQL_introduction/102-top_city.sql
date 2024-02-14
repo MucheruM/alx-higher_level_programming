991c2245314af1279cf1decb91d6ed39af4186e2
MucheruM <petermucheru420@gmail.com>
2024-02-14 12:25:56 -0800

    Write a script that displays the temp of top 3 ordered in DESC

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,8 @@
 -- Displays top 3 cities temp during July and August DESC
 SELECT city, AVG(value) AS avg_temp
 FROM `temperatures`
-WHERE month = 7 OR month = 8
+WHERE month IN (7, 8)
 GROUP BY `city`
-GROUP BY `avg_temp` DESC
+GROUP BY `avg_temp`
+DESC
 LIMIT 3;
