d26b241a29f94bc1bb734425b12fae031fbc01ed
MucheruM <petermucheru420@gmail.com>
2024-02-14 04:55:17 -0800

    Create a script that deletes a database without fail

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- This script removes a the database hbtn_0c_0
 -- If the database doesnot exist the script still continues
-DROP DATABASE IF NOT EXISTS hbtn_0c_0
+DROP DATABASE IF EXISTS hbtn_0c_0
