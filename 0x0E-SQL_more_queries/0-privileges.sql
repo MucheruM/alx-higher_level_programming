b801228f1dec5b4b5775f057043929003a19b7d0
MucheruM <petermucheru420@gmail.com>
2024-02-14 15:59:20 -0800

    Write a script that lists all priviliges of the MySQL users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- Lists all priviliges of the MySQL users
-SHOW GRANTS FOR IN '(user_0d_1, user_0d_2)'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
