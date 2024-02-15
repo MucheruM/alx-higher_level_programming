59caef6600e0be4fd306e20c75821d04a4319904
MucheruM <petermucheru420@gmail.com>
2024-02-14 17:17:14 -0800

    Write a script that creates the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,9 +3,8 @@
 -- The script should not fail if the user exists
 
 -- First create the server with the password
-CREATE USER
-       IF NOT EXISTS 'user_0d_1'@'localhost'
-       IDENTIFIED BY 'user_0d_1_pwd'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
 -- Give access to all priviliges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
