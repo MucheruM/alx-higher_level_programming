ecf0d6998835b173f08b5eb5252f91b5b5b68953
MucheruM <petermucheru420@gmail.com>
2024-02-15 17:43:19 -0800

    m

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,13 +3,13 @@
 SELECT tg.name
 FROM tv_genres tg
 WHERE tg.name NOT IN (
-      SELECT name
-      FROM tv_genres tg
-      LEFT JOIN tv_show_genres tsg
+    SELECT name
+    FROM tv_genres tg
+    LEFT JOIN tv_show_genres tsg
       	   	ON tg.id = tsg.genre_id
-      LEFT JOIN tv_shows ts
+    LEFT JOIN tv_shows ts
       	   	ON tsg.show_id = ts.id
-      WHERE ts.title = 'Dexter'
+    WHERE ts.title = 'Dexter'
 )
 GROUP BY tg.name
 GROUP BY tg.name;
