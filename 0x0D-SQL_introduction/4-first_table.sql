424505dc1b708b880bbf85ec9b4d2bb87a144cfd
MucheruM <petermucheru420@gmail.com>
2024-03-12 18:17:33 -0700

    Create a table first_table with id and name variables

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,7 @@
 -- Create a table called first_table in the current database
 
 -- If the table already exists the script should not fail
-CREATE TABLE first_table (
+CREATE TABLE IF NOT EXISTS first_table (
        id INT
        name VARCHAR(256)
        );
