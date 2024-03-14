2ced722405d9801dce08d5b5a9bf6081a7c1c3e8
MucheruM <petermucheru420@gmail.com>
2024-03-13 18:33:16 -0700

    SQL@

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- Lists all priviliges of the MySQL users 1 and 2
 SHOW GRANTS FOR 'user_od_1'@'localhost';
 SHOW GRANTS FOR 'user_od_2'@'localhost';
+
+FLUSH PRIVILIGES;
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Create the SQL server user_od_1 with all privilages
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
