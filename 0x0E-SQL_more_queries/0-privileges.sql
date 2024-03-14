87ded0c97979219c93fb6906c5af4cf72bc4fc5e
MucheruM <petermucheru420@gmail.com>
2024-03-13 18:36:18 -0700

    SQL2

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,6 @@
 -- Lists all priviliges of the MySQL users 1 and 2
-SHOW GRANTS FOR 'user_od_1'@'localhost';
-SHOW GRANTS FOR 'user_od_2'@'localhost';
 
-FLUSH PRIVILIGES;
+-- lists all privileges of the MySQL users user_0d_1
+SHOW GRANTS FOR 'user_od_1'@'localhost';
+-- lists all privileges of the MySQL users user_0d_2
+SHOW GRANTS FOR 'user_od_2'@'localhost';
