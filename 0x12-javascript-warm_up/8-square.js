3e5078def0bdeaf4deed8f9c7895c75163df69c7
MucheruM <petermucheru420@gmail.com>
2024-04-09 01:38:05 -0700

    Write a script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,11 +4,11 @@ const size = parseInt(process.argv[2], 10);
 
 if (!size) console.log('Missing size');
 else {
-    for (let i = 0; i < size; i++) {
-	let line = '';
-	for (let j = 0; j < size; j++) {
+  for (let i = 0; i < size; i++) {
+    let line = '';
+    for (let j = 0; j < size; j++) {
 	    line += 'X';
-	}
-	console.log(line);
     }
+    console.log(line);
+  }
 }
