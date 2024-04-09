f7c24501a9f89253cfa0366184c5fc6f63fa9ba4
MucheruM <petermucheru420@gmail.com>
2024-04-08 19:01:05 -0700

    Write a script that searches the second biggest integer in the list of arguments.

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+'use strict';
+
+function getSecondLargest (array) {
+    return (array.length > 1) ? array.sort((a, b) => b - a)[1] : 0;
+}
+
+function convertToIntegerArray (array) {
+    return array.map((num) => parseInt(num));
+}
+
+const array = convertToIntegerArray(process.argv.slice(2));
+
+console.log(getSecondLargest(array));
