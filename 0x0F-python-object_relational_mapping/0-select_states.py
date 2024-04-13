a23857fd8ca81622162cb740a7b965800c99b24e
MucheruM <petermucheru420@gmail.com>
2024-04-13 11:12:15 -0700

    Connect to a data base from a python code

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    username, password, db_name = sys.argv[1:]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        password=password,
+        database=db_name,
+        port=3306,
+        )
+
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        SELECT * FROM states
+        ORDER BY states.id;
+        """
+        )
+
+    result_set = cursor.fetchall()
+
+    for row in result_set:
+        print(row)
+
+    cursor.close()
+    conn.closed()
