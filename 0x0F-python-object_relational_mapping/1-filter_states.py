da897a251657b49bba548ca060e4d3dd0ab776d4
MucheruM <petermucheru420@gmail.com>
2024-04-13 15:20:28 -0700

    File

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+""" The script logs into MySQLdp and,
+    returns the names of state,
+    strting with upper N
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    username, password, db_name = sys.argv[1:]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        password=password,
+        database=db_name,
+        port=3306
+        )
+
+    cursor=conn.cursor()
+    cursor.execute(
+        """
+        SELECT*FROM states
+        WHERE name LIKE 'N%'
+        ORDER BY states.id:
+        """
+        )
+    result_set = cursor.fetchall()
+
+    for row in result_set:
+        print(row)
+
+    cursor.closed()
+    conn.closed()
