540420160abab118b579bd834a159aabe8cc3a6a
MucheruM <petermucheru420@gmail.com>
2024-04-15 07:48:21 -0700

    Write a script that takes an arg and displays all values in the states table of the db where name matches the argv.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+""" A script that takes in an argument and displays all values
+in the states table of the database where name matches the arg"""
+
+if __name__ == "__main__":
+        import sys
+        import MySQLdb
+
+        username, password, db_name, state_name = sys.argv[1:]
+
+        conn = MySQLdp.connect(
+            host="localhost",
+            user=username,
+            password=password,
+            database=db_name,
+            port=3306,
+        )
+
+        cursor = conn.cursor()
+        query = """
+            SELECT * FROM states
+            WHERE name LIKE BINARY 'Arizona%'
+            ORDER BY states.id
+        """.format(state_name)
+
+        cursur.execute(query)
+
+        return_set = cursor.fetchall()
+
+        for row in return_set:
+                print(row)
+
+        cursor.close()
+        conn.close()
+                                    
