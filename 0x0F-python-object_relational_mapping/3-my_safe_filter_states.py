39514ab82081ad599551139e7e907a7b4c3e210d
MucheruM <petermucheru420@gmail.com>
2024-04-17 03:10:04 -0700

    Write a script that is SQL injection secure but takes in arg and can search a MySQLdb

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+""" Writing a secure code from MySQL injection which takes and ar gets results from an SQL database"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    username, password, db_name, state_name = sys.argv[1:]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        password=password,
+        database=db_name,
+        port=3306,
+    )
+
+    cursor = conn.cursor()
+
+    query = """
+    SELECT * FROM states
+    WHERE name LIKE BINARY %s
+    ORDER BY states.id;
+    """
+
+    cursor.execute(query, (state_name + '%',))
+
+    return_set = cursor.fetchall()
+
+    for row in return_set:
+        print(row)
+
+    cursor.close()
+    conn.close()
