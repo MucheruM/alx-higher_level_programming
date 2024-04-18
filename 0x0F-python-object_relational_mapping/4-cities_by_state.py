401bcc0439ea9d7ddc68769e39db8797d8425f5c
MucheruM <petermucheru420@gmail.com>
2024-04-17 20:02:13 -0700

    A py script tha executes an SQL comand to search its database

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,10 +19,10 @@ if __name__ == "__main__":
     cursor = conn.cursor()
     cursor.execute(
         """
-        SELECT cities.id, cities.name, states.name
+        SELECT cities.id, cities.name, state.name
         FROM cities
-            JOIN states ON cities.state_id = states.id
-        ORDER BY cities.id;
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id
         """
     )
 
