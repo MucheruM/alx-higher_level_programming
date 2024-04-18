3a7e21e128144592f31a47019a9008b3d40cd1af
MucheruM <petermucheru420@gmail.com>
2024-04-17 20:31:07 -0700

    A py script that searches the MySQLdb and gives results

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         """
         SELECT cities.id, cities.name, states.name
         FROM cities
-            JOIN states ON cities.state_id = states.id
+            INNER JOIN states ON states.id=cities.state_id
         ORDER BY cities.id;
         """
     )
