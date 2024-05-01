2ad82aa63a70046ae19298e99b61726365378571
MucheruM <petermucheru420@gmail.com>
2024-04-30 19:51:48 -0700

    M

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that sends a POST request displaying the response
-curl -sX POST "$1" -d "email=test@gmail.com" -d "subject=I will always be there for PLD"
+curl -sX POST "$1" -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
